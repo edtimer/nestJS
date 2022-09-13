1839167a20ef84bcb4d2e18ab2f183304c197a3a
edtimer <b4timer@protonmail.com>
2022-09-13 11:31:42 +0100

    prisma service and module in src added with no spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule, BookmarkModule],
+  imports: [UserModule, AuthModule, BookmarkModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })

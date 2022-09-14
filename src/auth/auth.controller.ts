45719dd4dcf60d831f6f9c47875fde7e1fa731a5
edtimer <26600954+edtimer@users.noreply.github.com>
2022-09-14 08:46:57 +0100

    added class validator and class transformer dependency

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,25 @@
-import { Body, Controller, Get, ParseIntPipe, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import { Body, Controller, Get, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {
+  constructor(private readonly authService: AuthService) {}
 
-    }
-
-    @Post('signUp')
-    //dto means data transmission object which is a nest object that helps us in terms of validating data in body
-    signUp(
-    @Body('email') email: string,
+  @Post('signUp')
+  //dto means data transmission object which is a nest object that helps us in terms of validating data in body
+  signUp(
     //ParseIntPipe converts it to a number
-    @Body('password',ParseIntPipe) password: string,
-    dto: AuthDto) {
-        //would print the body
-        console.log(dto);
+    // @Body('password',ParseIntPipe) password: string,
+    @Body() dto: AuthDto,
+  ) {
+    //would print the body
+    console.log(dto);
 
-        return this.authService.signUp()
-    }
-    @Post('signIn')
-    signIn() {
-        return this.authService.signIn()
-    }
-
+    return this.authService.signUp();
+  }
+  @Post('signIn')
+  signIn() {
+    return this.authService.signIn();
+  }
 }

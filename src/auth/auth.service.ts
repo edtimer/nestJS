05febbe8d61d90a5622d960bcae95bd129c022ca
edtimer <26600954+edtimer@users.noreply.github.com>
2022-09-14 09:23:54 +0100

    sign up authentication logic with password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-
+import * as argon from 'argon2';
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signIn() {
     return { msg: 'this is the sign in' };
   }
-  signUp(dto: AuthDto) {
-    return { msg: 'this is the sign up' };
+  //async function since we will use prisma in creating a user
+  async signUp(dto: AuthDto) {
+    //generate password hash
+    const hash = await argon.hash(dto.password);
+    //save user to db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
+    });
+
+    //return the user
+    return user;
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,16 +3,14 @@ import { PrismaClient } from '@prisma/client';
 //here we have the logic connecting to db by extending prisma client
 @Injectable()
 export class PrismaService extends PrismaClient {
-constructor(){
-
+  constructor() {
     super({
-        datasources:{
-            db:{
-
-                url:'postgresql://database source here'
-            },
-        }
-    })
-}
-
+      datasources: {
+        db: {
+          //todo remove hardcoded dev db url
+          url: 'postgresql://postgres:441422@localhost:5432/nestdb?schema=public',
+        },
+      },
+    });
+  }
 }

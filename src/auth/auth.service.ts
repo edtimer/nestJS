65cb4f4655bfa5f620e0b32f67bb751d4bf6f64e
edtimer <26600954+edtimer@users.noreply.github.com>
2022-09-14 09:37:48 +0100

    added error handling for existing user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -13,15 +14,24 @@ export class AuthService {
     //generate password hash
     const hash = await argon.hash(dto.password);
     //save user to db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash,
-      },
-    });
-    delete user.hash;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+      delete user.hash;
 
-    //return the user
-    return user;
+      //return the user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ForbiddenException('user exists');
+      }
+    }
   }
 }
